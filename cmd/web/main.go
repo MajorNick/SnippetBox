cb9017e399b4136453147619342b0807c558d7c4
MajorNick <nika.chigladze11@gmail.com>
2023-02-12 13:31:18 +0400

    added errorlogers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,34 @@
 package main
+
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 func main() {
+
+	addr := flag.String("addr",":4000","HTTP Network Address")
+	flag.Parse()
+
+	errorlog := log.New(os.Stdout,"ERROR\t",log.Ldate|log.Ltime|log.Lshortfile)
+	infolog := log.New(os.Stdout,"INFO\t",log.Ldate|log.Ltime)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorlog,
+		Handler: mux,
+	}
 
 	mux.Handle("/static/",http.StripPrefix("/static",fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	infolog.Printf("Starting server on %s\n",*addr)
+	err := srv.ListenAndServe()
 	
-	log.Fatal(err)
+	errorlog.Fatal(err)
 }
